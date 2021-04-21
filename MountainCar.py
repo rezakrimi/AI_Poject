a9ce2ea4b1d7a2622903d4b556cf552fedac1f92
Reza Karimi <rezakrimi@gmail.com>
2021-04-20 20:19:25 -0400

    final version

--- MountainCar.py
+++ MountainCar.py
@@ -3,7 +3,6 @@ import numpy as np
 
 env = gym.make('MountainCar-v0')
 env.reset()
-
 LEARNING_RATE = .1
 DISCOUNT= .95
 EPISODES = 25000
@@ -13,9 +12,9 @@ SHOW_EVERY = 2000
 DISCRETE_OS_SIZE = [20] * len(env.observation_space.high)
 discrete_os_win_size = (env.observation_space.high- env.observation_space.low) / DISCRETE_OS_SIZE
 
-epsilon = 0.5
+epsilon = 0.8
 START_EPSILON_DECAYING = 1
-END_EPSILON_DECAYING = EPISODES // 2
+END_EPSILON_DECAYING = EPISODES // 1.5
 epsilon_decay_value = epsilon / (END_EPSILON_DECAYING - START_EPSILON_DECAYING)
 
 q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
@@ -48,7 +47,7 @@ for episode in range(EPISODES):
             new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
             q_table[discrete_state + (action, )] = new_q
         elif new_state[0] >= env.goal_position:
-            print('we made it in episode ' + str(episode))
+            print('Got it right on iteration ' + str(episode))
             q_table[discrete_state + (action, )] = 0
 
         discrete_state = new_discrete_state
